5e63171846cbfda5455848e8039e3c677ddc4965
Lucas Inacio <lucasinacio2003@hotmail.com>
2024-02-10 00:48:13 -0300

    Add language selector and overlay

--- scripts/script.js
+++ scripts/script.js
@@ -16,14 +16,42 @@ async function fetchLanguageData(lang) {
 }
 
 async function changeLanguage(lang) {
-    await setLanguagePreference(lang);
-    
+    setLanguagePreference(lang);
+    toggleLanguageOverlay();
+
     const langData = await fetchLanguageData(lang);
     updateContent(langData);
+    updateLanguageSelectorFlag(lang);
+}
+
+function toggleLanguageOverlay() {
+    var overlay = document.getElementById("languageOverlay");
+    overlay.style.width = (overlay.style.width === "100%") ? "0" : "100%";
+}
+
+function updateLanguageSelectorFlag(lang) {
+    const flagImageUrlMap = {
+        'en': './assets/images/us_flag.png',
+        'pt': './assets/images/br_flag.png'
+    };
+
+    const flagElement = document.querySelector('.selected-flag');
+
+    flagElement.src = flagImageUrlMap[lang];
+    flagElement.alt = lang === 'en' ? 'English' : 'Portuguese';
 }
 
 window.addEventListener('DOMContentLoaded', async () => {
     const userPreferredLanguage = localStorage.getItem('language') || 'en';
     const langData = await fetchLanguageData(userPreferredLanguage);
     updateContent(langData);
+});
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const userPreferredLanguage = localStorage.getItem('language') || 'en';
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    updateContent(langData);
+    
+    // Update the flag in the language selector
+    updateLanguageSelectorFlag(userPreferredLanguage);
 });
